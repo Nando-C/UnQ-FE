c80a8896adf0d309c2b4d2d9d407c4697f1e75e9
Nando-C <73954373+Nando-C@users.noreply.github.com>
2021-10-06 21:24:46 +0100

    adds search bar for the menu

--- src/components/Shops/ShopMenu/ShopMenu.tsx
+++ src/components/Shops/ShopMenu/ShopMenu.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { Card, Button, ListGroup,  } from "react-bootstrap"
+import { Card, Button, ListGroup, Row, Col, Form, FloatingLabel,  } from "react-bootstrap"
 import { useAppSelector } from "../../../redux/app/hooks"
 import { IMenu } from "../../../typings/menu"
 import Item from "../Item/Item"
@@ -12,6 +12,8 @@ interface ShopMenuProps {
 const ShopMenu = ({shopId}:ShopMenuProps) => {
 
     const shop = useAppSelector(state => state.shops.data.find(shop => shop._id === shopId))
+
+    const [query, setQuery] = useState("")
    
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
@@ -21,11 +23,24 @@ const ShopMenu = ({shopId}:ShopMenuProps) => {
     return (
         <>
             <Card.Title className="m-5"> MENU </Card.Title>
+            <Row>
+                <Col>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <FloatingLabel controlId="floatingInput" label="Search Menu">
+                                <Form.Control
+                                    placeholder="Search Menu"
+                                    value={query}
+                                    onChange={(e) => setQuery(e.target.value)}
+                                />
+                            </FloatingLabel>
+                        </Form.Group>
+                </Col>
+            </Row>
             <div className="my-3 text-align-end">
                 <Button onClick={handleShow}>Add Menu Item</Button>
             </div>
             <ListGroup className="px-0" variant="flush">
-                {shop?.menu.map(menuItem => (
+                {shop?.menu.filter(menu => (menu.category.toLowerCase().includes(query.toLowerCase()) || menu.name.toLowerCase().includes(query.toLowerCase()) || menu.description.toLowerCase().includes(query.toLowerCase()))).map(menuItem => (
                     <ListGroup.Item className="px-0 menu-item">
                         <Item itemId={menuItem._id} shopId={shopId} />
                     </ListGroup.Item>
