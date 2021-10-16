e5d098f3886013bf2a4dfe36cd01164e38b784b4
Nando-C <73954373+Nando-C@users.noreply.github.com>
2021-10-16 13:47:52 +0100

    converts CheckOut into an offCanvas-> from bottom

--- src/components/Customers/CheckOut/CheckOut.tsx
+++ src/components/Customers/CheckOut/CheckOut.tsx
@@ -1,4 +1,4 @@
-import { Modal, Button } from "react-bootstrap"
+import { Modal, Button, Offcanvas } from "react-bootstrap"
 import "./CheckOut.css"
 import { PayPalButton } from "react-paypal-button-v2"
 import { useEffect, useState } from "react"
@@ -22,7 +22,39 @@ const CheckOut = ({show, handleClose, total }: CheckOutProps) => {
 
     return(
         <>
-            <Modal show={show} onHide={handleClose}>
+            <div className="CheckOut">
+            <Offcanvas placement="bottom" show={show} onHide={handleClose}>
+                <Offcanvas.Header closeButton>
+                    <Offcanvas.Title>Select Payment Type</Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body >
+                    <PayPalButton 
+                        amount={totalPayment}
+                        // currency="GBP"
+                        onSuccess={(details: any, data: object) => {
+                            console.log("cart : ", cart)
+                            {cart.split.filter(i => i.splitStatus === "open").map(item => dispatch(checkOutItem({
+                                    shopId: "",
+                                    tableId: cart.tableId,
+                                    cartId: cart._id,
+                                    item: item
+                                }))
+                            )}
+                            // alert("Transaction completed by " + details.payer.name.given_name)
+                            
+                            handleClose()
+                            return
+                        }}
+                        options={{
+                            clientId: "sb",
+                            currency: "GBP"
+                        }}
+                    />
+                </Offcanvas.Body>
+            </Offcanvas>
+            </div>
+
+            {/* <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
                     <Modal.Title>Select Payment Type</Modal.Title>
                 </Modal.Header>
@@ -50,7 +82,7 @@ const CheckOut = ({show, handleClose, total }: CheckOutProps) => {
                         }}
                     />
                 </Modal.Body>
-            </Modal>
+            </Modal> */}
         </>
     )
 }
