39e8c2c3ab7febee8256ba738120bc15db4bc725
Nando-C <73954373+Nando-C@users.noreply.github.com>
2021-10-18 17:21:26 +0100

    adds style to All Shop List components

--- src/components/Customers/AllShopsList/AllShopsList.tsx
+++ src/components/Customers/AllShopsList/AllShopsList.tsx
@@ -1,54 +1,57 @@
-import { useState } from "react"
-import { Col, FloatingLabel, Form, ListGroup, Row } from "react-bootstrap"
-import { Link } from "react-router-dom"
-import { useAppSelector } from "../../../redux/app/hooks"
-import { selectShopsData } from "../../../redux/slices/shopSlice"
-import ShopCard from "../../Shops/ShopCard/ShopCard"
-import CustomerShopCard from "../CustomerShopCard/CustomerShopCard"
-import "./AllShopsList.css"
+import { useState } from "react";
+import { Col, FloatingLabel, Form, ListGroup, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useAppSelector } from "../../../redux/app/hooks";
+import { selectShopsData } from "../../../redux/slices/shopSlice";
+import ShopCard from "../../Shops/ShopCard/ShopCard";
+import CustomerShopCard from "../CustomerShopCard/CustomerShopCard";
+import "./AllShopsList.css";
 
 const AllShopsList = () => {
-    
-    const shopList = useAppSelector(selectShopsData)
+  const shopList = useAppSelector(selectShopsData);
 
-    const [query, setQuery] = useState("")
+  const [query, setQuery] = useState("");
 
-    return(
-        <>
-            <Row>
-                <Col>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <FloatingLabel controlId="floatingInput" label="Search Shop">
-                            <Form.Control
-                                placeholder="Search Shop"
-                                value={query}
-                                onChange={(e) => setQuery(e.target.value)}
-                            />
-                        </FloatingLabel>
-                    </Form.Group>
-                </Col>
-            </Row>
-        <ListGroup className="ShopList mb-5" variant="flush">
-            {shopList.filter(shop => (
-                        shop.name.toLowerCase().includes(query.toLowerCase())
-                        || shop.bio.toLowerCase().includes(query.toLowerCase())
-                        || shop.menu.find(m => m.name.toLowerCase().includes(query.toLowerCase()))
-                    )
-                ).map((shop) => (
-                    <ListGroup.Item className="px-0" key={shop._id}>
-                        <Link to={`/customer/shop/${shop._id}`}>
-                            <CustomerShopCard
-                                // _id={shop._id}
-                                name={shop.name}
-                                cover={shop.cover}
-                                bio={shop.bio}
-                            />
-                        </Link>
-                    </ListGroup.Item>
-            ))}
-        </ListGroup>
-        </>
-    )
-}
+  return (
+    <div className="bg-light">
+      <Row>
+        <Col>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <FloatingLabel controlId="floatingInput" label="Search Shop">
+              <Form.Control
+                placeholder="Search Shop"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+              />
+            </FloatingLabel>
+          </Form.Group>
+        </Col>
+      </Row>
+      <ListGroup className="ShopList mb-5" variant="flush">
+        {shopList
+          .filter(
+            (shop) =>
+              shop.name.toLowerCase().includes(query.toLowerCase()) ||
+              shop.bio.toLowerCase().includes(query.toLowerCase()) ||
+              shop.menu.find((m) =>
+                m.name.toLowerCase().includes(query.toLowerCase())
+              )
+          )
+          .map((shop) => (
+            <ListGroup.Item className="px-0" key={shop._id}>
+              <Link to={`/customer/shop/${shop._id}`}>
+                <CustomerShopCard
+                  // _id={shop._id}
+                  name={shop.name}
+                  cover={shop.cover}
+                  bio={shop.bio}
+                />
+              </Link>
+            </ListGroup.Item>
+          ))}
+      </ListGroup>
+    </div>
+  );
+};
 
-export default AllShopsList
+export default AllShopsList;
--- src/components/Customers/CustomerShopCard/CustomerShopCard.tsx
+++ src/components/Customers/CustomerShopCard/CustomerShopCard.tsx
@@ -1,37 +1,35 @@
-import { Card, Col, Image, Row } from "react-bootstrap"
-import "./CustomerShopCard.css"
+import { Card, Col, Image, Row } from "react-bootstrap";
+import "./CustomerShopCard.css";
 
 interface ShopCardProps {
-    name: string
-    cover: string
-    bio: string
+  name: string;
+  cover: string;
+  bio: string;
 }
 
 const CustomerShopCard = ({
-    // _id,
-    name,
-    cover,
-    bio,
+  // _id,
+  name,
+  cover,
+  bio,
 }: ShopCardProps) => {
-    return(
-        <>
-        <Card className="CustomerShopCard">
-                <Row className="text-start">
-                    <Col xs={3}>
-                        <Image src={cover} rounded fluid />
-                    </Col>
-                    <Col className="pt-2" xs={9}>
-                        <Card.Subtitle>
-                            {name}
-                        </Card.Subtitle>
-                        <Card.Body className="p-0">
-                            <small>{bio}</small>
-                        </Card.Body>
-                    </Col>
-                </Row>
-            </Card>
-        </>
-    )
-}
+  return (
+    <>
+      <Card className="CustomerShopCard">
+        <Row className="text-start">
+          <Col className="image-col" xs={4}>
+            <Image src={cover} rounded fluid className="w-100 h-100" />
+          </Col>
+          <Col className="pt-2" xs={8}>
+            <Card.Subtitle>{name}</Card.Subtitle>
+            <Card.Body className="p-0">
+              <small>{bio}</small>
+            </Card.Body>
+          </Col>
+        </Row>
+      </Card>
+    </>
+  );
+};
 
-export default CustomerShopCard
+export default CustomerShopCard;
