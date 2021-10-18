66f214745ba1fdb1844517b9460838b77d0a11d4
Nando-C <73954373+Nando-C@users.noreply.github.com>
2021-10-18 16:28:18 +0100

    adds style to Cart components

--- src/components/Customers/Cart/Cart.tsx
+++ src/components/Customers/Cart/Cart.tsx
@@ -1,76 +1,85 @@
-import { useEffect, useState } from "react"
-import { Card, Col, Container, Image, Row, Button } from "react-bootstrap"
-import { IoArrowBack} from "react-icons/io5"
-import { useHistory, useParams } from "react-router"
-import { useAppDispatch, useAppSelector } from "../../../redux/app/hooks"
-import { getMyOpenCart, getTableCart, selectCartPointers, selectCartsData } from "../../../redux/slices/cartSlice"
-import { selectSelectedShop } from "../../../redux/slices/shopSlice"
-import CartListItems from "../CartListItems/CartListItems"
-import "./Cart.css"
+import { useEffect, useState } from "react";
+import { Card, Col, Container, Image, Row, Button } from "react-bootstrap";
+import { IoArrowBack } from "react-icons/io5";
+import { useHistory, useParams } from "react-router";
+import { useAppDispatch, useAppSelector } from "../../../redux/app/hooks";
+import {
+  getMyOpenCart,
+  getTableCart,
+  selectCartPointers,
+  selectCartsData,
+} from "../../../redux/slices/cartSlice";
+import { selectSelectedShop } from "../../../redux/slices/shopSlice";
+import CartListItems from "../CartListItems/CartListItems";
+import "./Cart.css";
 
 const Cart = () => {
+  const dispatch = useAppDispatch();
+  const { shopId, tableId } = useAppSelector(selectCartPointers);
+  const [tableName, setTableName] = useState("");
 
-    const dispatch = useAppDispatch()
-    const { shopId, tableId } = useAppSelector(selectCartPointers)
-    const [tableName, setTableName] = useState('')
+  const shop = useAppSelector(selectSelectedShop);
 
-    const shop = useAppSelector(selectSelectedShop)
-   
-    useEffect(()=> {
-        // dispatch(getMyOpenCart())
-        dispatch(getTableCart(tableId))
-        if (tableId) {
-            const tName = shop!.tables.filter(table => table._id === tableId)[0].name
-            setTableName(tName)
-            console.log(tName)
-        }
-    }, [])
-
-    const cart = useAppSelector(selectCartsData)
-    // console.log(cart)
-
-    const history = useHistory()
-    const goBack = () => {
-        history.goBack()
+  useEffect(() => {
+    // dispatch(getMyOpenCart())
+    dispatch(getTableCart(tableId));
+    if (tableId) {
+      const tName = shop!.tables.filter((table) => table._id === tableId)[0]
+        .name;
+      setTableName(tName);
+      console.log(tName);
     }
-    
-    return (
-        <>
-            <Container className="Cart">
-                <Row className="align-items-center">
-                    <Col xs={2}>
-                        <Button onClick={goBack}>
-                            <IoArrowBack size={25} />
-                        </Button>
-                    </Col>
-                    {tableId &&
-                        <Col xs={8}>
-                            <Card.Subtitle>
-                                Table: {tableName}
-                            </Card.Subtitle>
-                        </Col>
-                    }
-                </Row>
-                { 
-                cart.items.length > 0 
-                    ? <CartListItems />
-                    : 
-                        <Card>
-                            <Card.Body>
-                                <Row className="align-items-center">
-                                    <Col className="image-col pe-0 ">
-                                        <Image src="https://nayemdevs.com/wp-content/uploads/2020/03/default-product-image.png" rounded fluid />
-                                    </Col>
-                                    <Card.Title>
-                                        Looks like you don't have any open orders!!
-                                    </Card.Title>
-                                </Row>
-                            </Card.Body>
-                        </Card>
-                }
-            </Container>
-        </>
-    )
-}
+  }, []);
 
-export default Cart
+  const cart = useAppSelector(selectCartsData);
+  // console.log(cart)
+
+  const history = useHistory();
+  const goBack = () => {
+    history.goBack();
+  };
+
+  return (
+    <>
+      <Container className="Cart bg-light">
+        <Row className="align-items-center">
+          <Col xs={2}>
+            <Button onClick={goBack}>
+              <IoArrowBack size={25} />
+            </Button>
+            {/* <span>Back</span> */}
+          </Col>
+          {tableId && (
+            <Col className="text-center" xs={8}>
+              <Card.Subtitle className="cart-table-name">
+                <span>Table: {tableName}</span>
+              </Card.Subtitle>
+            </Col>
+          )}
+        </Row>
+        {cart.items.length > 0 ? (
+          <CartListItems />
+        ) : (
+          <Card>
+            <Card.Body>
+              <Row className="align-items-center">
+                <Col className="image-col pe-0 ">
+                  <Image
+                    src="https://nayemdevs.com/wp-content/uploads/2020/03/default-product-image.png"
+                    rounded
+                    fluid
+                  />
+                </Col>
+                <Card.Title>
+                  Looks like you don't have any open orders!!
+                </Card.Title>
+              </Row>
+            </Card.Body>
+          </Card>
+        )}
+      </Container>
+    </>
+  );
+};
+
+export default Cart;
--- src/components/Customers/CartItem/CartItem.tsx
+++ src/components/Customers/CartItem/CartItem.tsx
@@ -1,244 +1,308 @@
-import { useEffect, useState } from "react"
-import { Card, Col, Row, Button, Image, Form, Badge } from "react-bootstrap"
-import { useParams } from "react-router"
-import { useAppDispatch, useAppSelector } from "../../../redux/app/hooks"
-import { addToCart, addToSplit, decreaseSplit, deleteFromCart, getTableCart, removeFromCart, removeFromSplit, selectCartsData } from "../../../redux/slices/cartSlice"
-import { IItem, ISplitItem } from "../../../typings/cart"
-import { IMenu } from "../../../typings/menu"
-import { RiDeleteBinLine } from "react-icons/ri"
-import "./CartItem.css"
+import { useEffect, useState } from "react";
+import { Card, Col, Row, Button, Image, Form, Badge } from "react-bootstrap";
+import { useParams } from "react-router";
+import { useAppDispatch, useAppSelector } from "../../../redux/app/hooks";
+import {
+  addToCart,
+  addToSplit,
+  decreaseSplit,
+  deleteFromCart,
+  getTableCart,
+  removeFromCart,
+  removeFromSplit,
+  selectCartsData,
+} from "../../../redux/slices/cartSlice";
+import { IItem, ISplitItem } from "../../../typings/cart";
+import { IMenu } from "../../../typings/menu";
+import { RiDeleteBinLine } from "react-icons/ri";
+import "./CartItem.css";
 
 interface CartItemProps {
-    cartId: string
-    itemId: string | undefined
-    selectedAll: boolean
-    setSelectedAll: React.Dispatch<React.SetStateAction<boolean>>
+  cartId: string;
+  itemId: string | undefined;
+  selectedAll: boolean;
+  setSelectedAll: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const CartItem = ({cartId, itemId, selectedAll, setSelectedAll}: CartItemProps) => {
+const CartItem = ({
+  cartId,
+  itemId,
+  selectedAll,
+  setSelectedAll,
+}: CartItemProps) => {
+  const params = useParams<{ shopId: string; tableId: string }>();
+  const shopId = params.shopId;
+  const tableId = params.tableId;
 
-    const params = useParams<{ shopId: string, tableId: string }>()
-    const shopId  = params.shopId
-    const tableId  = params.tableId
+  const cart = useAppSelector(selectCartsData);
+  const cartItem = cart.items.find((item) => item._id === itemId);
+  const splitItem = cart.split.find(
+    (item) => item._id === itemId && item.splitStatus === "open"
+  );
+  const payed = cartItem!.qtyPayed;
+  const itemQty = cartItem!.qty - payed!;
 
-    const cart = useAppSelector(selectCartsData)
-    const cartItem = cart.items.find(item => item._id === itemId)
-    const splitItem = cart.split.find(item => (item._id === itemId && item.splitStatus === "open"))
-    const payed = cartItem!.qtyPayed
-    const itemQty = cartItem!.qty-payed!
+  const [selected, setSelected] = useState(false);
 
-    const [selected, setSelected] = useState(false)
+  const [split, setSplit] = useState<ISplitItem | undefined>({
+    _id: "",
+    userId: "",
+    menuId: null,
+    qty: 0,
+    splitStatus: "",
+  });
 
-    const [split, setSplit] = useState<ISplitItem | undefined>({
-        _id: '',
-        userId: '',
-        menuId: null,
-        qty: 0,
-        splitStatus: '',
-    })
+  const [item, setItem] = useState<IItem | undefined>({
+    _id: "",
+    menuId: null,
+    qty: 1,
+    qtyPayed: 0,
+  });
 
-    const [item, setItem] = useState<IItem | undefined>({
-        _id: "",
-        menuId: null,
-        qty: 1,
-        qtyPayed: 0,
-    })
+  useEffect(() => {
+    dispatch(getTableCart(tableId));
+    setItem(cartItem);
+    setSplit(splitItem);
+  }, []);
 
-    useEffect(() => {
-        dispatch(getTableCart(tableId))
-        setItem(cartItem)
-        setSplit(splitItem)
-    }, [])
+  useEffect(() => {
+    setItem(cartItem);
+    setSplit(splitItem);
+    if (splitItem) {
+      setSelected(true);
+    } else {
+      setSelected(false);
+    }
+  }, [cartItem, splitItem]);
 
-    useEffect(() => {
-        setItem(cartItem)
-        setSplit(splitItem)
-        if (splitItem) {
-            setSelected(true)
-        } else {
-            setSelected(false)
-        }
-    }, [cartItem, splitItem])
+  useEffect(() => {
+    if (selectedAll) {
+      increment();
+    } else {
+      removeItemFromSplit();
+    }
+  }, [selectedAll]);
 
-    useEffect(()=> {
-        if(selectedAll) {
-            increment()
-        } else {
-            removeItemFromSplit()
-        }
-    }, [selectedAll])
+  const dispatch = useAppDispatch();
 
-    const dispatch = useAppDispatch()
+  const handleSelect = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (selectedAll) {
+      setSelectedAll(false);
+      setSelected(false);
+    } else {
+      setSelected(!selected);
+    }
+    const splitExists = item?.menuId._id === split?.menuId._id;
 
-    const handleSelect = (e: React.ChangeEvent<HTMLInputElement>) => {
-        if(selectedAll) {
-            setSelectedAll(false)
-            setSelected(false)
-        } else {
-            setSelected(!selected)
-        }
-        const splitExists = item?.menuId._id === split?.menuId._id
-        
-        console.log("splitExists: ", splitExists)
-        
-        if(!selected && !splitExists) {
-            console.log("add to split")
-            increment()
-        } else if (selected && splitExists) {
-            console.log("remove from split")
-            removeItemFromSplit()
-        }
+    console.log("splitExists: ", splitExists);
+
+    if (!selected && !splitExists) {
+      console.log("add to split");
+      increment();
+    } else if (selected && splitExists) {
+      console.log("remove from split");
+      removeItemFromSplit();
+    }
+  };
+
+  const increment = () => {
+    //<====================   adds item to SPLIT Cart
+    let quantity = 0;
+
+    if (selectedAll) {
+      if (split) {
+        quantity = item!.qty - item!.qtyPayed! - split.qty;
+        console.log("split -> quantity: ", quantity);
+      } else {
+        quantity = item!.qty - item!.qtyPayed!;
+        console.log("selectedAll -> quantity: ", quantity);
+      }
+    } else {
+      quantity = 1;
+      console.log("quantity: ", quantity);
     }
 
-    const increment = () => {   //<====================   adds item to SPLIT Cart
-        let quantity = 0
+    const plusItem = {
+      ...item!,
+      qty: quantity,
+    };
 
-        if (selectedAll){
-            if(split) {
-                quantity =item!.qty - item!.qtyPayed! - split.qty 
-                console.log("split -> quantity: ", quantity)
-            } else {
-                quantity = item!.qty - item!.qtyPayed! 
-                console.log("selectedAll -> quantity: ", quantity)
-            }
-        } else {
-            quantity = 1
-            console.log("quantity: ", quantity)
-        }
-        
-        const plusItem = {
-            ...item!,
-            qty: quantity
-        }
-
-       dispatch(addToSplit({
+    dispatch(
+      addToSplit({
         shopId: shopId,
         tableId: tableId,
         cartId: cartId,
-        item: plusItem
-       }))
+        item: plusItem,
+      })
+    );
+  };
 
-    }
+  const decrement = () => {
+    const minusItem = item!;
 
-    const decrement = () => {
-      const minusItem = item!
-        
-       dispatch(decreaseSplit({
+    dispatch(
+      decreaseSplit({
         shopId: shopId,
         tableId: tableId,
         cartId: cartId,
-        item: minusItem
-       }))
-    }
+        item: minusItem,
+      })
+    );
+  };
 
-    const removeItemFromSplit = () => {
-        const minusItem = item!
-        dispatch(removeFromSplit({
-            shopId: shopId,
-            tableId: tableId,
-            cartId: cartId,
-            item: minusItem
-           }))
-    }
+  const removeItemFromSplit = () => {
+    const minusItem = item!;
+    dispatch(
+      removeFromSplit({
+        shopId: shopId,
+        tableId: tableId,
+        cartId: cartId,
+        item: minusItem,
+      })
+    );
+  };
 
-    const deleteItemFromCart = () => {
-        dispatch(deleteFromCart({
-            shopId: shopId,
-            tableId: tableId,
-            item: item!,
-        }))
-    }
+  const deleteItemFromCart = () => {
+    dispatch(
+      deleteFromCart({
+        shopId: shopId,
+        tableId: tableId,
+        item: item!,
+      })
+    );
+  };
 
-    return(
-        <>
-            <Row className="align-items-center">
-                <Col className="d-flex p-0" xs={1}>
-                    <Form.Group className="ms-3 text-center" controlId="formBasicCheckbox">
-                        <Form.Check 
-                            type="checkbox" 
-                            checked={selected} 
-                            onChange={(e) => handleSelect(e)} 
-                        />
-                        {/* {console.log("selected: ",selected)} */}
-                    </Form.Group>
-                </Col>
-                <Col xs={11}>
-                    <Card className="CartItem">
-                    <Badge  bg={splitItem?.qty === itemQty ? "success" : "warning"} text="dark">
-                            <small> Cart Qty: {splitItem?.qty ? itemQty - splitItem.qty : itemQty}</small>
-                        </Badge>
-                        <Row className="align-items-center">
-                            <Col className="image-col pe-0 " xs={4}>
-                                <Image src={item?.menuId?.image} rounded fluid />
-                            </Col>
-                            <Col xs={8} className="text-start ps-2">
-                                <Row className="my-3">
-                                    <Col>
-                                        <Card.Subtitle className="ms-3">
-                                            {item?.menuId?.name}
-                                        </Card.Subtitle>
-                                        {/* <Card.Body className="ms-3 p-0">
-                                    <small>{item?.menuId?.short_description}</small>
-                                </Card.Body> */}
-                                    </Col>
-                                    {/* <Col>
-                                        <Badge pill bg="warning" text="dark">
-                                            <small> Cart Qty: {splitItem?.qty ? itemQty - splitItem.qty : itemQty}</small>
-                                        </Badge>
-                                        <Card.Text>
-                                            <small> Cart Qty: {splitItem?.qty ? itemQty - splitItem.qty : itemQty}</small>
-                                        </Card.Text>
-                                    </Col> */}
-                                </Row>
-                                <Row className="pb-1 align-items-center">
-                                    <Col className="p-0" xs={4}>
-                                        <Card.Body className="p-0 ms-3">
-                                            <strong>
-                                                £ {item?.menuId?.price}
-                                            </strong>
-                                        </Card.Body>
-                                    </Col>
-                                    <Col xs={6}>
-                                        <Row className="text-center align-items-center">
-                                            <Col className="px-0">
-                                                {selected || selectedAll
-                                                    ? <Button active size="sm" onClick={decrement} ><strong>-</strong></Button>
-                                                    : <></>
-                                                    // <Button disabled size="sm" onClick={decrement} ><strong>-</strong></Button>
-                                                }
-                                            </Col>
-                                            <Col className="px-0">
-                                                <Card.Body className="p-0">
-                                                    <small>{split?.qty}</small>
-                                                </Card.Body>
-                                            </Col>
-                                            <Col className="px-0">
-                                                {selected || selectedAll
-                                                    ? <Button active size="sm" onClick={increment} ><strong>+</strong></Button>
-                                                    : <Button size="sm" variant="danger" onClick={deleteItemFromCart}><RiDeleteBinLine/></Button>
-                                                }
-                                                {/* <Button size="sm" onClick={decrement} ><strong>-</strong></Button> */}
-                                            </Col>
-                                        </Row>
-                                    </Col>
-                                    {/* <Col className="p-0" xs={1}>
+  return (
+    <>
+      <Row className="align-items-center">
+        <Col className="d-flex p-0" xs={1}>
+          <Form.Group
+            className="ms-3 text-center"
+            controlId="formBasicCheckbox"
+          >
+            <Form.Check
+              type="checkbox"
+              checked={selected}
+              onChange={(e) => handleSelect(e)}
+            />
+            {/* {console.log("selected: ",selected)} */}
+          </Form.Group>
+        </Col>
+        <Col xs={11}>
+          <Card className="CartItem">
+            <span className="cart-qty-span">
+              <small>
+                in cart: {splitItem?.qty ? itemQty - splitItem.qty : itemQty}
+              </small>
+            </span>
+            {/* <Badge
+              bg={splitItem?.qty === itemQty ? "success" : "warning"}
+              text="dark"
+            >
+              <small>
+                {" "}
+                Cart Qty: {splitItem?.qty ? itemQty - splitItem.qty : itemQty}
+              </small>
+            </Badge> */}
+            <Row>
+              <Col className="image-col pe-0 " xs={4}>
+                <Image
+                  src={item?.menuId?.image}
+                  rounded
+                  fluid
+                  className="w-100 h-100"
+                />
+              </Col>
+              <Col xs={8} className="text-start ps-2">
+                <Row className="my-3">
+                  <Col>
+                    <Card.Subtitle className="ms-3">
+                      {item?.menuId?.name}
+                    </Card.Subtitle>
+                    {/* <Card.Body className="ms-3 p-0">
+                        <small>{item?.menuId?.short_description}</small>
+                    </Card.Body> */}
+                  </Col>
+                  {/* <Col>
+                    <Badge pill bg="warning" text="dark">
+                        <small> Cart Qty: {splitItem?.qty ? itemQty - splitItem.qty : itemQty}</small>
+                    </Badge>
+                    <Card.Text>
+                        <small> Cart Qty: {splitItem?.qty ? itemQty - splitItem.qty : itemQty}</small>
+                    </Card.Text>
+                  </Col> */}
+                </Row>
+                <Row className="pb-1 align-items-center">
+                  <Col className="p-0" xs={4}>
+                    <Card.Body className="p-0 ms-3">
+                      <span className="price text-primary">
+                        £ {item?.menuId?.price}
+                      </span>
+                    </Card.Body>
+                  </Col>
+                  <Col xs={6}>
+                    <Row className="quantity-wrapper text-center align-items-center">
+                      <Col className="px-0">
+                        {
+                          selected || selectedAll ? (
+                            <Button active size="sm" onClick={decrement}>
+                              <strong>-</strong>
+                            </Button>
+                          ) : (
+                            <></>
+                          )
+                          // <Button disabled size="sm" onClick={decrement} ><strong>-</strong></Button>
+                        }
+                      </Col>
+                      <Col className="px-0">
+                        <Card.Body className="p-0">
+                          <small>{split?.qty}</small>
+                        </Card.Body>
+                      </Col>
+                      <Col className="px-0">
+                        {selected ? (
+                          (splitItem?.qty
+                            ? itemQty - splitItem.qty
+                            : itemQty) <= 0 ? (
+                            <Button disabled size="sm" onClick={increment}>
+                              <strong>+</strong>
+                            </Button>
+                          ) : (
+                            <Button active size="sm" onClick={increment}>
+                              <strong>+</strong>
+                            </Button>
+                          )
+                        ) : (
+                          <Button
+                            size="sm"
+                            variant="danger"
+                            onClick={deleteItemFromCart}
+                          >
+                            <RiDeleteBinLine />
+                          </Button>
+                        )}
+                        {/* <Button size="sm" onClick={decrement} ><strong>-</strong></Button> */}
+                      </Col>
+                    </Row>
+                  </Col>
+                  {/* <Col className="p-0" xs={1}>
                                         {selected
                                             ? <Button disabled size="sm" variant="danger" onClick={deleteItemFromCart}><RiDeleteBinLine/></Button>
                                             : <Button active size="sm" variant="danger" onClick={deleteItemFromCart}><RiDeleteBinLine/></Button>
                                         } */}
-                                        {/* <Button size="sm" variant="danger" onClick={deleteItemFromCart}><RiDeleteBinLine/></Button> */}
-                                    {/* </Col> */}
-                                </Row>
-                            </Col>
-                        </Row>
-                        {/* <Badge  bg="warning" text="dark">
+                  {/* <Button size="sm" variant="danger" onClick={deleteItemFromCart}><RiDeleteBinLine/></Button> */}
+                  {/* </Col> */}
+                </Row>
+              </Col>
+            </Row>
+            {/* <Badge  bg="warning" text="dark">
                             <small> Cart Qty: {splitItem?.qty ? itemQty - splitItem.qty : itemQty}</small>
                         </Badge> */}
-                    </Card>
-                </Col>
-            </Row>
-        </>
-    )
-}
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
 
-export default CartItem
+export default CartItem;
